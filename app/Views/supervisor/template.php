f8ac34528da67f6b565f9d2faaab12c63f87480b
danuri <danualbantani@gmail.com>
2025-12-01 12:11:23 +0700

    favicon

--- app/Views/supervisor/template.php
+++ app/Views/supervisor/template.php
@@ -18,7 +18,7 @@
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="https://kemenag.go.id/assets/imgs/theme/favicon.png" />
+    <link rel="icon" type="image/x-icon" href="https://kemenag.go.id/favicon.ico" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
